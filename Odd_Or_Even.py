0222d6cc93db1e4858a970947496934fcf1133fb
AnitaWe <anitaogsandra@gmail.com>
2022-03-30 10:13:41 +0200

    Update Odd_Or_Even.py

--- Odd_Or_Even.py
+++ Odd_Or_Even.py
@@ -1,8 +1,13 @@
 #Ask a user and ask for a number and tell if it's a odd or even number
 
-number = int(input("Write a number to see if it's odd or even? "))
 
-if(number % 2 == 0):
-    print(number , " is an even number")
-else:
-    print(number, " is an odd number")
+def main():
+    x = int(input("Write a number to see if it's odd or even? "))
+    print(x , " is an even number" if is_even(x) else " is an odd number")
+
+
+def is_even(n):
+    return n % 2 == 0
+
+if __name__ == "__main__":
+    main()
